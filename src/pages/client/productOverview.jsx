4479d18b4a126f4a47fb41a9b78c6d5ed43cee00
vidulanka <vidulankapasindu2001@gmail.com>
2025-06-21 03:06:30 +0530

    Day 17 ✏️

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 import Loader from "../../components/loader";
+import ImageSlider from "../../components/imageSlider";
+import getCart, { addToCart } from "../../utils/cart";
 
 export default function ProductOverview() {
     const params=useParams()
@@ -13,11 +15,12 @@ export default function ProductOverview() {
     const[status,setStatus]=useState("loading")
    useEffect(
        () => {
+        console.log((import.meta.env.VITE_BACKEND_URL+"/api/product/"+params.id))
         if(status=="loading"){
            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product/"+params.id).then(
                (res) => {
                   console.log(res);
-                  setProduct(res.data)
+                  setProduct(res.data.product)
                   setStatus("loaded")
                }
            ).catch(
@@ -33,20 +36,68 @@ export default function ProductOverview() {
 
     return (
         <div className="w-full h-full">
-         {
-            status=="loading "&&<Loader/>
-         }
-         {
-            status=="loaded "&& 
-                <div className="w-full h-full">
-                    Product Loaded
+              {
+                 status=="loading " &&<Loader/>
+              }
+              
+          {
+               status === "loaded" && 
+               <div className="w-full h-full flex">
+                   <div className="w-[50%] h-full ">
+                    <ImageSlider images={product.images}/>
+      
                 </div>
-         }
+               <div className="w-[50%] h-full p-[40px] ">
+                 <h1 className="text-3xl font-bold text-center mb-[40px]">{product.name}{" | "}<span className="text-2xl mr-[20px]"> <h2 className="text-3xl mr-[20px] text-gray-500"> {product.altNames.join("|")}</h2> </span> </h1>
+                 <h2 className="text-2xl mr-[20px]"></h2>
+                    
+
+                 <div className="w-full flex justify-center mb-[40px] ">
+                    {product.lebeledPrice>product.price?(
+                        <>
+                        <h2 className="text-2xl mr-[20px]">LKR:{product.price.toFixed(2)}</h2>
+                        <h2 className="text-2xl line-through">LKR:{product.lebeledPrice.toFixed(2)}</h2>
+                        
+                      
+                        
+                        
+                        </>
+                    ):(
+                          <h2 className="text-2xl mr-[20px]">{product.price}</h2>
+                        
+                   ) }
+                 </div>
+                 <p className="text-xl text-center text-gray-500 mb-[40px]">
+                    {product.description}
+                 </p>
+                 <div className="w-full flex justify-center mb-[40px]">
+                    <button className="bg-pink-800 border cursor-pointer border-pink-800 text-white w-[200px] h-[50px] rounded-lg hover:bg-white hover:text-pink-800 text-2xl">Buy Now</button>
+                    <button className="bg-pink-800  border cursor-pointer border-pink-800 text-white w-[200px] h-[50px] rounded-lg hover:bg-white hover:text-pink-800 text-2xl ml-[20px]"
+                    
+                    onClick={() => {
+                        addToCart(product,1);
+                        toast.success(" Product Added to cart");
+                        console.log(getCart())
+                    }
+                    
+                    
+                    }>
+                        Add to Cart
+                    </button>
+                    
+                    </div> 
+                
+             </div>
+         </div>
+}
+
+
+
          {
             status=="error"&&<div>
                 ERROR
                 </div>
          }
         </div>
-    );
+    )
 }
---
+++ src/utils/cart.js
@@ -0,0 +1,53 @@
+export default function getCart(){
+    let cart= localStorage.getItem("cart");
+
+    if( cart==null){
+    cart=[]
+    localStorage.setItem("cart",JSON.stringify(cart));
+    return[]
+    }
+
+    cart=JSON.parse(cart)
+    return cart
+   
+
+}
+
+export function addToCart(product,qty){
+    let cart = getCart();
+
+    const productIndex = cart.findIndex((prdct)=>prdct.productID===product.productID);
+    if(productIndex == -1){
+        cart.push(
+            {
+                productID:product.productID,
+                name:product.name,
+                altNames:product.altNames,
+                price:product.price,
+                lebeledPrice:product.lebeledPrice,
+                images:product.images[0],
+                quantity:qty
+            
+            }
+        )
+
+
+    }else{
+        cart[productIndex].quantity += qty
+        if(cart[productIndex].quantity<=0){
+            cart = cart.filter((prdct)=>prdct.productID !== product.productID)
+        }
+    }
+
+    localStorage.setItem("cart",JSON.stringify(cart));
+    return cart
+
+ 
+}
+
+export function removeFromCart(productID){
+    let cart = getCart();
+    cart = cart.filter((product)=>product.productID !== productID);
+    localStorage.setItem("cart",JSON.stringify(cart));
+    return cart
+}
